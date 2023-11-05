e28289b68c06439e6f3d4378af26456ea72592a4
Dzulfiqar <103058254+DzulfiqarSiraj@users.noreply.github.com>
2023-11-05 17:47:20 +0700

    Add files via upload

--- tugas4.js
+++ tugas4.js
@@ -1,13 +1,13 @@
 function sortedArray(arr){
 
-  let arrTemp = arr;
+  const arrTemp = [...arr]; //shallow copy
 
   for(let i = 0; i < arrTemp.length; i++){
     for(let j = i; j < arrTemp.length; j++){
       if(arrTemp[i] <= arrTemp[j]){
         arrTemp[i] = arrTemp[i]
       } else {
-        let num = arrTemp[i]
+        const num = arrTemp[i]
         arrTemp[i] = arrTemp[j]
         arrTemp[j] = num
       }
@@ -16,6 +16,17 @@ function sortedArray(arr){
   
   return arrTemp
 }
+
+let arrayAcak = [6,3,7,5,2,9,0,3]
+let arrNew = sortedArray(arrayAcak)
+console.log(arrayAcak)
+console.log(arrNew)
+const array = [2,4,5,7,8,4];
+const addArray = [1,2,3,19,10,40];
+for(let i=0; i<addArray.length;i++){
+  array[array.length] = addArray[i];
+}
+console.log(array)
 
 function divideAndSort(num){
   let strNum = num + "";
@@ -40,6 +51,29 @@ function divideAndSort(num){
   console.log(hasil)
 }
 
+function divideAndSort2(num){
+  let strNum = num + "";
+  hasil = ""
+  arrTemp = [];
+  
+  for(let i = 0; i < strNum.length; i++){
+    arrTemp.push(strNum[i])
+    if(strNum[i] === "0" || i === strNum.length - 1){
+      arrTemp.sort((a,b) => a-b);
+      arrTemp.forEach(item =>{
+        if(item === "0"){
+          hasil += ""
+        }else{
+          hasil += item;
+        }
+      })
+      arrTemp = []
+    }
+  }
+
+  hasil = Number(hasil)
+  console.log(hasil)
+}
+
 divideAndSort(5956560159466056)
-
-
+divideAndSort2(5956560159466056)
